f676ee7db5dec0061111cf2c5370d63a3e457b7c
sim <git@sgougeon.fr>
2024-12-21 11:26:53 +0000

    Move redis import under feature flag

--- autoendpoint/src/server.rs
+++ autoendpoint/src/server.rs
@@ -13,11 +13,10 @@ use serde_json::json;
 
 #[cfg(feature = "bigtable")]
 use autopush_common::db::bigtable::BigTableClientImpl;
+#[cfg(feature = "redis")]
+use autopush_common::db::redis::RedisClientImpl;
 use autopush_common::{
-    db::{
-        client::DbClient, redis::RedisClientImpl, spawn_pool_periodic_reporter, DbSettings,
-        StorageType,
-    },
+    db::{client::DbClient, spawn_pool_periodic_reporter, DbSettings, StorageType},
     middleware::sentry::SentryWrapper,
 };
 
